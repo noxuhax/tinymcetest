3da7f87dfa15b0909cee266015058edab6f07b92
noxuhax <noxuhax@gmail.com>
2026-01-27 17:34:40 +0400

    Configure external premium plugins

--- src/main.js
+++ src/main.js
@@ -9,15 +9,25 @@ import "tinymce/plugins/lists";
 import "tinymce/plugins/table";
 
 import "tinymce-premium/plugins/advcode";
+import "tinymce-premium/plugins/licensekeymanager";
 
 import "./styles.css";
+
+const premiumPluginBase = new URL(
+  "tinymce-premium/plugins/",
+  import.meta.url,
+).href;
 
 tinymce.init({
   selector: "#editor",
   license_key: "",
   height: 500,
   menubar: "file edit view insert format tools table help",
-  plugins: ["advcode", "link", "lists", "table"],
+  external_plugins: {
+    advcode: `${premiumPluginBase}advcode/plugin.min.js`,
+    licensekeymanager: `${premiumPluginBase}licensekeymanager/plugin.min.js`,
+  },
+  plugins: ["licensekeymanager", "advcode", "link", "lists", "table"],
   toolbar:
     "undo redo | blocks | bold italic underline | alignleft aligncenter alignright | bullist numlist | table | advcode",
 });
